d42880c9cedd19644ea65e240afe621276f30c37
willian condori <willianangelcondorifarfan@gmail.com>
2023-09-06 12:13:22 -0500

    prueba

--- login/index.php
+++ login/index.php
@@ -26,52 +26,33 @@
             <div class="row">
                 <div class="col-12">
                     <div class="d-flex align-items-center min-vh-100">
-                        <div class="w-100 d-block bg-white shadow-lg rounded my-5">
+                        <div class="w-100 d-block bg-white shadow-lg rounded my-5"> 
                             <div class="row">
-                                <div class="col-lg-5 d-none d-lg-block bg-login rounded-left"></div>
+                                <div class="col-lg-5 d-none d-lg-block rounded-left">
+                                 <img src="../imagen/logo.png" width="440px" height="365px" alt="">
+                                </div>
                                 <div class="col-lg-7">
                                     <div class="p-5">
-                                        <div class="text-center mb-5">
-                                            <a href="index.html" class="text-dark font-size-22 font-family-secondary">
-                                                <i class="mdi mdi-alpha-x-circle"></i> <b>XACTON</b>
-                                            </a>h
-                                        </div>
-                                        <h1 class="h5 mb-1">¡Bienvenido de nuevo!</h1>
+                                      
+                                        <h1 class="h5 mb-1">Bienvenidos!</h1>
                                         <p class="text-muted mb-4">Ingrese su dirección de correo electrónico y contraseña para acceder al panel de administración.</p>
                                         <form class="user">
                                             <div class="form-group">
-                                                <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Direccion de correo Electronico">
+                                                <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Ingrese su Email">
                                             </div>
                                             <div class="form-group">
-                                                <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Contraseña">
+                                                <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Ingrese su Password">
                                             </div>
                                             <a href="" class="btn btn-success btn-block waves-effect waves-light"> Ingresar </a>
 
-                                            <div class="text-center mt-4">
-                                                <h5 class="text-muted font-size-16">Iniciar sesión usando</h5>
-                                            
-                                                <ul class="list-inline mt-3 mb-0">
-                                                    <li class="list-inline-item">
-                                                        <a href="javascript: void(0);" class="social-list-item border-primary text-primary"><i class="mdi mdi-facebook"></i></a>
-                                                    </li>
-                                                    <li class="list-inline-item">
-                                                        <a href="javascript: void(0);" class="social-list-item border-danger text-danger"><i class="mdi mdi-google"></i></a>
-                                                    </li>
-                                                    <li class="list-inline-item">
-                                                        <a href="javascript: void(0);" class="social-list-item border-info text-info"><i class="mdi mdi-twitter"></i></a>
-                                                    </li>
-                                                    <li class="list-inline-item">
-                                                        <a href="javascript: void(0);" class="social-list-item border-secondary text-secondary"><i class="mdi mdi-github-circle"></i></a>
-                                                    </li>
-                                                </ul>
-                                            </div>
+                                      
                                             
                                         </form>
 
                                         <div class="row mt-4">
                                             <div class="col-12 text-center">
-                                                <p class="text-muted mb-2"><a href="pages-recoverpw.html" class="text-muted font-weight-medium ml-1">¿Olvidaste tu contraseña?</a></p>
-                                                <p class="text-muted mb-0">¿No tienes una cuenta? <a href="pages-register.html" class="text-muted font-weight-medium ml-1"><b>Inscribirse</b></a></p>
+                                                <p class="text-muted mb-2"><a href="pages-recoverpw.html" class="text-muted font-weight-medium ml-1">Olvidaste tu Contraseña?</a></p>
+                                                
                                             </div> <!-- end col -->
                                         </div>
                                         <!-- end row -->
