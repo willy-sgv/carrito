12c87181667f93ab2c5a5e273810e15385667c03
willian condori <willianangelcondorifarfan@gmail.com>
2023-10-27 09:10:05 -0500

    Update sistema.php

--- sistema.php
+++ sistema.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VetMay</title>
+    <title>WILLY</title>
 
     <link href="plantilla/Admin/vertical/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="plantilla/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
