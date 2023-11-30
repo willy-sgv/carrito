6c2f8fead52c06326e438847efa89e7f56147977
willian condori <willianangelcondorifarfan@gmail.com>
2023-11-30 08:08:20 -0500

    nuevos

--- proveedores.php
+++ proveedores.php
@@ -18,65 +18,52 @@ include('include/menu.php');
 <!--INICIO DE CONTENIDO-->
 <div class="main-content">
     <div class="page-content">
-        <div class="container-fluid"> <!--conteiner solo se usa el 80% de la pantalla conteiner-fluid ocupa todo-->
-            <div class="row"><!--row ayuda a ser responsive pero en bootstrap-->
+        <div class="container-fluid">
+            <div class="row">
                 <div class="col-12">
-                <?php include("include/modal_regis_provee.php"); ?> 
-                                       <h4>LISTADO DE PEDIDOS</h4>
+                    <?php include("include/modal_regis_provee.php"); ?> 
+                    <h4>LISTADO DE PEDIDOS</h4>
                     <div class="card">
                         <div class="card-body">
-                            
-                        <table id="basic-datatable" class="table dt-responsive nowrap">
-                            <thead>
-                                <tr>
-                                    
-                                    <th>RUC</th>
-                                    <th>RAZON SOCIAL</th>
-                                    <th>EMAIL</th>
-                                    <th>TELEFONO</th>
-                                    <th>DIRECCION</th>
-                                    <th>METODO DE PAGO</th>
-                                  
-                                </tr>
+                            <table id="basic-datatable" class="table dt-responsive nowrap">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>RUC</th>
+                                        <th>RAZON SOCIAL</th>
+                                        <th>CORREO</th>
+                                        <th>TELEFONO</th>
+                                        <th>DIRECCION</th>
+                                        <th>METODO DE PAGO</th>
+                                        <th>ACCIONES</th>
+                                    </tr>
                                 </thead>
                                 <tbody>
                                     <?php
-                                    $consulta= "SELECT * FROM proveedor";
-                                    $ejecutar= mysqli_query($conexion, $consulta);
-                                    $contador= 0;
-                                    while ($respuesta= mysqli_fetch_array($ejecutar)) {
-                                        $contador= 1;
-                                        
-                                        echo "<br>";
+                                    $consulta = "SELECT * FROM proveedor";
+                                    $ejecutar = mysqli_query($conexion, $consulta);
+                                    $contador = 1; // Inicializar el contador fuera del bucle
+
+                                    while ($respuesta = mysqli_fetch_array($ejecutar)) {
+                                        echo "<tr>";
                                         echo "<td>".$contador."</td>";
-                                        echo "<td>".$respuesta['id_cliente']."</td>";
-                                        echo "<td>".$respuesta['fecha_hora_pedido']."</td>";
-                                        echo "<td>".$respuesta['fecha_entrega']."</td>";
+                                        echo "<td>".$respuesta['id']."</td>";
+                                        echo "<td>".$respuesta['ruc']."</td>";
+                                        echo "<td>".$respuesta['razon_social']."</td>";
+                                        echo "<td>".$respuesta['correo']."</td>";
+                                        echo "<td>".$respuesta['telefono']."</td>";
+                                        echo "<td>".$respuesta['direccion']."</td>";
                                         echo "<td>".$respuesta['metodo_pago']."</td>";
-                                        echo "<td>".$respuesta['monto']."</td>";
-                                        echo "<td>".$respuesta['comprobante']."</td>";
-                                        echo "<td>".$respuesta['estado']."</td>";
-                                       
-                                        
-                                        echo "<br><button class='btn btn-success'>Editar</button><button class='btn btn-danger'>Eliminar</button>";
+                                        echo "<td><button class='btn btn-success'>Editar</button><button class='btn btn-danger'>Eliminar</button></td>";
+                                        echo "</tr>";
 
-
-
-                                        echo "</br>";
+                                        $contador++; // Incrementar el contador en cada iteración
                                     }
-
-
-
                                     ?>
-
-            
                                 </tbody>
-                           
-                        </table>
-                           
-                        </div>  
+                            </table>
+                        </div>
                     </div>
-               
                 </div>
             </div>
         </div>
@@ -84,14 +71,13 @@ include('include/menu.php');
 </div>
 <!--FIN DE CONTENIDO-->
 
+<script src="plantilla/Admin/vertical/assets/js/jquery.min.js"></script>
+<script src="plantilla/Admin/vertical/assets/js/bootstrap.bundle.min.js"></script>
+<script src="plantilla/Admin/vertical/assets/js/metismenu.min.js"></script>
+<script src="plantilla/Admin/vertical/assets/js/waves.js"></script>
+<script src="plantilla/Admin/vertical/assets/js/simplebar.min.js"></script>
 
-    <script src="plantilla/Admin/vertical/assets/js/jquery.min.js"></script>
-    <script src="plantilla/Admin/vertical/assets/js/bootstrap.bundle.min.js"></script>
-    <script src="plantilla/Admin/vertical/assets/js/metismenu.min.js"></script>
-    <script src="plantilla/Admin/vertical/assets/js/waves.js"></script>
-    <script src="plantilla/Admin/vertical/assets/js/simplebar.min.js"></script>
-
-    <!-- App js -->
-    <script src="plantilla/Admin/vertical/assets/js/theme.js"></script>
+<!-- App js -->
+<script src="plantilla/Admin/vertical/assets/js/theme.js"></script>
 </body>
 </html>
