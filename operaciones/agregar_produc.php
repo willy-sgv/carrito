0d62c34e132ab708e14298711291cdc7722ae122
willian condori <willianangelcondorifarfan@gmail.com>
2023-12-11 17:43:31 -0500

    nnn

--- operaciones/agregar_produc.php
+++ operaciones/agregar_produc.php
@@ -13,11 +13,11 @@ if ($contar_r > 0) {
     $array_productos[$id] = 1;
 
     if (array_key_exists($array_productos[$id],$array_productos)) {
-        $array_productos [$id] = 1;
+        $array_productos [$id] += 1;
 
     }else {
         $array_productos[$id] = 1;
     }
 }
-
+print_r($array_productos);
 ?>
